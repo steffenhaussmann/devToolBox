def9c3f31c950ac6135a406d4fc778b28678541d
Steffen Haußmann <steffen_haussmann@h-team.de>
2024-12-18 22:31:11 +0100

    feat: angular ssr installed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
-  providers: [...(appConfig.providers || []), provideAnimations()],
+  providers: [...(appConfig.providers || []), provideAnimations(), provideClientHydration()],
 }).catch((err) => console.error(err));
